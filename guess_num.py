efeb1d625934049792219c07e82636c5ae9b0a13
Erika Wu <ironance@hotmail.com>
2019-04-05 15:54:03 +0800

    Add showing th e number of guess after BINGO

--- guess_num.py
+++ guess_num.py
@@ -1,12 +1,15 @@
 import random
 
 ans = random.randint(1,100)
+guess_count = 0
 
 while True:
     user_ans = input('Please input your answer: ')
     user_ans = int(user_ans)
+    guess_count = guess_count + 1
     if ans == user_ans:
         print('BINGO! The answer is', ans)
+        print('You have guessed', guess_count, 'time(s)')
         break
     else:
         if ans > user_ans:
