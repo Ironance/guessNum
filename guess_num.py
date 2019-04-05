200abff4c05f6b5f214d7524b6bc2021c02e8dc8
Erika Wu <ironance@hotmail.com>
2019-04-05 16:25:00 +0800

    Add guess limit(not allow to guess the number out of range)

--- guess_num.py
+++ guess_num.py
@@ -19,16 +19,23 @@ while True:
     user_ans = input('Please input your answer: ')
     user_ans = int(user_ans)
     guess_count = guess_count + 1
-    if ans == user_ans:
-        print('BINGO! The answer is', ans)
-        print('You have guessed', guess_count, 'time(s)')
-        break
+    if user_ans > limit_up or user_ans < limit_low:
+        print('Guess', guess_count, ':')
+        print('The number is out of range!')
     else:
-        if ans > user_ans:
-            print('The answer is greater than', user_ans)
-            limit_low = user_ans + 1
-            print('Range of the remained numbers is', limit_low, 'to', limit_up)
+        if ans == user_ans:
+            print('Guess', guess_count, ':')
+            print('BINGO! The answer is', ans)
+            print('You have guessed', guess_count, 'time(s)')
+            break
         else:
-            print('The answer is less than', user_ans)
-            limit_up = user_ans - 1
-            print('Range of the remained numbers is', limit_low, 'to', limit_up)
+            if ans > user_ans:
+                print('Guess', guess_count, ':')
+                print('The answer is greater than', user_ans)
+                limit_low = user_ans + 1
+                print('Range of the remained numbers is', limit_low, 'to', limit_up)
+            else:
+                print('Guess', guess_count, ':')
+                print('The answer is less than', user_ans)
+                limit_up = user_ans - 1
+                print('Range of the remained numbers is', limit_low, 'to', limit_up)
